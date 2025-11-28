a316ebe6105ee3b06d0260959e45556e19c75dac
Aaisha <ayesha.naaz2499@gmail.com>
2025-11-28 09:46:04 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -12,7 +12,8 @@ st.set_page_config(
     layout="wide"
 )
 
-API_URL = "https://esg-model.onrender.com"  # your deployed FastAPI endpoint
+API_URL = "API_URL = "https://esg-model.onrender.com/predict"
+  # your deployed FastAPI endpoint
 
 # ----------------- UTILS -----------------
 def call_esg_api(payload: dict):
